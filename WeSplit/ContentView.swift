b8d8fb0d054e1e89d4c1b141678e4b8f564a9368
Tony Langworthy <langworthytech@gmail.com>
2023-07-30 19:40:37 -0500

    Sorting

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,17 +10,28 @@ import SwiftUI
 struct ContentView: View {
     @State private var tapCount = 0
     @State private var name = ""
+    let students = ["Tony", "Brandy", "Madison", "Brayden"]
+    @State private var selectedStudent = "Tony"
+
     
     var body: some View {
         
         NavigationStack {
             Form {
-                Button("Tap Count: \(tapCount)") {
-                    self.tapCount += 1
+                var sortedMadisonFirst = students.sorted(by: studentFirstSorted)
+                var sortedStudents = students.sorted()
+                
+                Picker("Select Student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                Section {
+                    ForEach(sortedStudents, id: \.self) {
+                        Text($0)
+                    }
                 }
                 
-                TextField("Enter your name", text: $name)
-                Text("Hello, \(name)!")
             }
 
         }.navigationTitle("SwiftUI")
@@ -31,4 +42,14 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+func studentFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Madison" {
+        return true
+    } else if name2 == "Madison" {
+        return false
+    }
+
+    return name1 < name2
 }
